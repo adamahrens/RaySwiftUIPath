50ed4a13cab8661164a350807895fe560c8cb36d
Adam Ahrens <adam.aahrens@gmail.com>
2021-04-13 20:36:17 -0500

    Adding initial project

---
+++ RayWenderlichSwiftUI/RayWenderlichSwiftUI/RayWenderlichSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RayWenderlichSwiftUIApp.swift
+//  RayWenderlichSwiftUI
+//
+//  Created by Adam Ahrens on 4/13/21.
+//
+
+import SwiftUI
+
+@main
+struct RayWenderlichSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
