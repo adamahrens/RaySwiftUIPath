9b3ee080d4b93e3869856fe26ddbc9c2acb5db1e
Adam Ahrens <adam.aahrens@gmail.com>
2021-04-19 19:45:15 -0500

    Adding New Project for Navigation work

---
+++ RayWenderlichNavigation/RayWenderlichNavigation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RayWenderlichNavigation
+//
+//  Created by Adam Ahrens on 4/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RayWenderlichNavigation/RayWenderlichNavigation/RayWenderlichNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  RayWenderlichNavigationApp.swift
+//  RayWenderlichNavigation
+//
+//  Created by Adam Ahrens on 4/19/21.
+//
+
+import SwiftUI
+
+@main
+struct RayWenderlichNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
